a507029b46e1d0564bdc0f55598e35fe5155ec0d
BowenYeboy30 <114602941+BowenYeboy30@users.noreply.github.com>
2025-11-24 23:19:03 -0500

    Update DetailsScreen.tsx

--- src/screens/DetailsScreen.tsx
+++ src/screens/DetailsScreen.tsx
@@ -1,25 +1,27 @@
 import * as React from "react";
-import { View, Text, Button, StyleSheet, ImageBackground } from "react-native";
+import { View, Text, Button, StyleSheet } from "react-native";
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import { RootStackParamList } from "../navigation/types";
 
 type Props = NativeStackScreenProps<RootStackParamList, "Details">;
-
 export default function DetailsScreen({ route, navigation }: Props) {
   const { itemId } = route.params;
 
   return (
-
     <View style={styles.container}>
-      <Text style={styles.boxtext}>Board the PRT 71C {"\n"}Millvale bus</Text>
-      <View style={{ height: 560 }} />
-              <Text style={styles.boxtext1}>9:39         30         9.9{"\n"}Arrival     Min       Miles </Text>
-      <View style={{ height: 10 }} />
-      <Button title="Go back" onPress={() => navigation.goBack()} />
-      <View style={{ height: 12 }} />
+      <View style={styles.box}>
+        <Text style={styles.boxtext}>Board the PRT 71C {"\n"}Millvale bus</Text>
+      </View>
 
+      <View style={{ height: 593 }} />
+
+      <View style={styles.boxRow}>
+        <Text style={styles.boxtext1}>
+          9:39         30         9.9{"\n"}Arrival     Min       Miles
+        </Text>
+        <Button title="Go back" onPress={() => navigation.goBack()} />
+      </View>
     </View>
-
   );
 }
 
@@ -27,7 +29,20 @@ const styles = StyleSheet.create({
   container: { flex: 1, padding: 20, gap: 12, justifyContent: "center" },
   title: { fontSize: 28, fontWeight: "700" },
   paragraph: { fontSize: 16, lineHeight: 22, opacity: 0.8 },
-  boxtext: {fontSize: 28},
-  boxtext1: {fontSize: 20, }
-
+  box: {
+    borderRadius: 20,
+    padding: 15,
+    justifyContent: "center",
+    backgroundColor: "#D3D3D3",
+  },
+  boxRow: {
+    borderRadius: 20,
+    padding: 12,
+    backgroundColor: "#D3D3D3",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+  boxtext: { fontSize: 28 },
+  boxtext1: { fontSize: 20, flex: 1 },
 });
